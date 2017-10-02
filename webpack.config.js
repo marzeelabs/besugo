dd320bc8cb3d120bf20c71acddfab28711250aa7
Luís Miguel <luism@marzeelabs.org>
2017-10-02 11:58:32 +0100

    feature/#7 reorganize output js files

--- webpack.config.js
+++ webpack.config.js
@@ -5,57 +5,50 @@ const webpack = require("webpack");
 // webpack config reference:
 //  https://webpack.js.org/configuration/
 
-const jsWebpacks = {
-  watch: process.env.SERVE === 'true',
-
-  // configuration regarding modules
-  module: {
-    // rules for modules (configure loaders, parser options, etc.)
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [ "env" ]
-          }
-        }
-      }
-    ]
-  },
-
-  // additional plugins
-  plugins: [
-    // babel-minify seems to have trouble with nested $'s, something in babel-plugin-minify-mangle-names
-    // for example the following jQuery would parse error when minifying with it:
-    // $a.b(function() { $('foo'); })
-    new webpack.optimize.UglifyJsPlugin()
-  ]
-};
-
 module.exports = [
-  Object.assign({
+  {
     // Here the application starts executing and webpack starts bundling
-    entry: glob.sync("./js/site/**/*.js"),
+    entry: {
+      js: glob.sync("./js/site/**/*.js"),
+      admin: glob.sync("./js/admin/**/*.js")
+    },
 
     // options related to how webpack emits results
     output: {
       // the target directory for all output files
       // must be an absolute path (use the Node.js path module)
-      path: path.resolve(__dirname, "./static/js/"),
+      path: path.resolve(__dirname, "./static/"),
 
       // the filename template for entry chunks
-      filename: "site.min.js"
+      filename: "[name]/main.min.js"
     },
-  }, jsWebpacks),
 
-  Object.assign({
-    entry: glob.sync("./js/admin/**/*.js"),
+    // Watch for changes and rebuild output files as necessary
+    watch: process.env.SERVE === 'true',
 
-    output: {
-      path: path.resolve(__dirname, "./static/admin/"),
-      filename: "admin.min.js"
+    // configuration regarding modules
+    module: {
+      // rules for modules (configure loaders, parser options, etc.)
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [ "env" ]
+            }
+          }
+        }
+      ]
     },
-  }, jsWebpacks)
+
+    // additional plugins
+    plugins: [
+      // babel-minify seems to have trouble with nested $'s, something in babel-plugin-minify-mangle-names
+      // for example the following jQuery would parse error when minifying with it:
+      // $a.b(function() { $('foo'); })
+      new webpack.optimize.UglifyJsPlugin()
+    ]
+  }
 ];
