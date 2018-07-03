6994d57b5f3cd1a99790db34198c69714793f122
Luís Miguel <luism@marzeelabs.org>
2018-07-03 19:00:29 +0100

    #80 Fix markdown in Person pages

--- components/people/Person.jsx
+++ components/people/Person.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import BesugoComponent from 'Besugo';
+import ReactHtmlParser from 'react-html-parser';
 
 import EndFooter from 'partials/EndFooter';
 import SocialIcons from 'partials/SocialIcons';
@@ -19,6 +20,11 @@ export default class Person extends BesugoComponent {
     };
   }
 
+  static extraProps(props, xplaceholder) {
+    const content = xplaceholder.getChildren('Content');
+    props.content = JSON.parse(content[0].text());
+  }
+
   getData() {
     if (this.isPreview()) {
       const { entry } = this.props;
@@ -30,7 +36,13 @@ export default class Person extends BesugoComponent {
       };
     }
 
-    return this.props;
+    const data = Object.assign({}, this.props);
+
+    // "Content" comes pre-built with HTML markup already.
+    // We need to parse it so that it doesn't show up as simple text
+    data.content = ReactHtmlParser(data.content);
+
+    return data;
   }
 
   renderBlock() {
